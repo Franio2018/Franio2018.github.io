2d7a80eca66d11a3e81734cc7cfefbbfd23a9877
Franio2018 <84621749+Franio2018@users.noreply.github.com>
2023-03-15 13:10:28 +0100

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,40 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+loadSprite("background","kartka_swiatecna.png")
+loadSprite("star","gwiazda2.png")
+loadSound("music", "Do szopy hej pasterze.mp3")
+
+// add a piece of text at position (120, 80)
+add([
+    text("hello"),
+    pos(120, 80),
+]);
+
+add([
+    sprite("background"),
+    pos(0,0)
+])
+const star = add([
+    sprite("star"),
+    pos(533,45),
+    rotate(1)
+])
+add([
+    play("music")
+])
+
+var xMinus = 1;
+onUpdate(()=>{
+    if(star.pos.x == 400)
+    {
+        xMinus = 1;
+    }
+    if(star.pos.x == 633)
+    {
+        xMinus = -1;    
+    }
+    star.pos.x += xMinus
+})
